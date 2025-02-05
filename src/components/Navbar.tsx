68de57e29c24e71b997f89d9a909f3e1b194a700
adinugroh0 <aditwist1@gmail.com>
2025-02-05 18:22:18 +0700

    logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,12 +18,9 @@ function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
   return (
     <div
-      className={cn(
-        "fixed top-10 inset-x-0 max-w-2xl mx-auto z-50",
-        className
-      )}>
+      className={cn("fixed top-0 inset-x-0 max-w-2xl mx-auto z-50", className)}>
       <Menu setActive={setActive}>
-        <MenuItem setActive={setActive} active={active} item="Services">
+        <MenuItem setActive={setActive} active={active} item="About me">
           <div className="flex flex-col space-y-4 text-sm">
             <HoveredLink href="/">Web </HoveredLink>
             <HoveredLink href="/#pengalaman">Experience</HoveredLink>
